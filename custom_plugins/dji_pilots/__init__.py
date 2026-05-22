69804c39b6031eedd2cc71b5d7a1ecc211122641
Arnaud M. <arnaud.choupinette@mailops.fr>
2026-05-22 13:38:03 +0200

    Ruff lint
    Signed-off-by: Arnaud M. <arnaud.choupinette@mailops.fr>

--- custom_plugins/dji_pilots/__init__.py
+++ custom_plugins/dji_pilots/__init__.py
@@ -1,27 +1,40 @@
-''' Pilot Prefered Band Selector '''
+"""Pilot Prefered Band Selector."""
 
 import logging
+
 from eventmanager import Evt
-from RHUI import UIField, UIFieldType, UIFieldSelectOption
+from RHUI import UIField, UIFieldSelectOption, UIFieldType
 
 logger = logging.getLogger(__name__)
 
-class PilotPreferedBandSelector():
-    def __init__(self, rhapi):
+
+class PilotPreferedBandSelector:
+    """Pilot Prefered Band Selector."""
+
+    def __init__(self, rhapi: any) -> None:
+        """Init."""
         self._rhapi = rhapi
         self.enabled = True
 
-    def initialize(self, _args):
-        logger.info('Initializing Pilot Prefered Band Selector')
-        options=[
-            UIFieldSelectOption('raceband', "RACEBAND"),
-            UIFieldSelectOption('dji', "DJI"),
+    def initialize(self, _args: any) -> None:
+        """Init wrapper."""
+        logger.info("Initializing Pilot Prefered Band Selector")
+        options = [
+            UIFieldSelectOption("raceband", "RACEBAND"),
+            UIFieldSelectOption("dji", "DJI"),
         ]
         self._rhapi.fields.register_pilot_attribute(
-            UIField('prefered_band', "Prefered Band", UIFieldType.SELECT,
-                    options=options, value='raceband'))
+            UIField(
+                "prefered_band",
+                "Prefered Band",
+                UIFieldType.SELECT,
+                options=options,
+                value="raceband",
+            )
+        )
 
-def initialize(rhapi):
+
+def initialize(rhapi: any) -> None:
+    """Init plugin."""
     s = PilotPreferedBandSelector(rhapi)
     rhapi.events.on(Evt.STARTUP, s.initialize)
-
